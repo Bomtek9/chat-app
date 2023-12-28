6a7b295ab408cd4f8328c965e2b7041183ab5221
Bomtek9 <clayduplantis9@gmail.com>
2023-12-27 21:25:48 -0600

    Added Firebase auth

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import Start from "./components/Start.js";
 import Chat from "./components/Chat.js";
 import { LogBox } from "react-native";
@@ -9,36 +9,41 @@ import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-//create the navigator
+// Create the navigator
 const Stack = createNativeStackNavigator();
 
+// Ignore error message in Welcome Screen
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZmWrPIFDNB1LdnAq198HzMGoli9zWuy8",
+  authDomain: "chat-app-5ff52.firebaseapp.com",
+  projectId: "chat-app-5ff52",
+  storageBucket: "chat-app-5ff52.appspot.com",
+  messagingSenderId: "15301083737",
+  appId: "1:15301083737:web:d7464de29030b1a993a748",
+  measurementId: "G-CQ6NKVFDCJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth with AsyncStorage
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 const App = () => {
-  // ignores error Message in Welcome Screen
-  LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCZmWrPIFDNB1LdnAq198HzMGoli9zWuy8",
-    authDomain: "chat-app-5ff52.firebaseapp.com",
-    projectId: "chat-app-5ff52",
-    storageBucket: "chat-app-5ff52.appspot.com",
-    messagingSenderId: "15301083737",
-    appId: "1:15301083737:web:d7464de29030b1a993a748",
-    measurementId: "G-CQ6NKVFDCJ",
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore(app);
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Chat">
-          {(props) => <Chat db={db} {...props} />}
-          {/* Using {...props} ensures that not only the db prop but also other relevant navigation-related props are passed to the Chat component. <Stack.Screen name="Chat" component={Chat} wouldnt pass the db props */}
+          {(props) => <Chat auth={auth} db={db} {...props} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
